00544eb919f85b444d0f09888cd57b2c4b839d43
Marcus Melo <marcus.melo@dcomp.ufs.br>
2023-04-24 11:55:44 -0300

    implementado naive bayes com o paradigma de POO

--- Teste-naive-bayes.py
+++ Teste-naive-bayes.py
@@ -9,123 +9,153 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import OneHotEncoder
 
+FILE_DADOS_BASE = 'tennis.csv'
+FILE_DADOS_TREINADOS = 'tennis_modelo.pkl'  # arquivo onde será salvo o modelo treinado
+
+
 class IO:
-  """
-  Classe para representar as operações de IO
-  """
-
-  def readDataCSV():
-      dataBase = pd.read_csv('/content/tennis.csv')
-      return dataBase
-
-
-  def salveDataTrain():
     """
-    Salvar a base de dados treinada para evitar reprocessamento (salva em binário)
+    Classe para representar as operações de IO
     """
-    with open('tennis.pkl', 'wb') as f:
-      pickle.dump([X_attr_train, Y_class_train], f)
+    @staticmethod
+    def readDataCSV():
+        dataBase = pd.read_csv(FILE_DADOS_BASE)
+        return dataBase
 
+    @staticmethod
+    def salveDataTrain(x_atributos, y_classes, model, encoder):
+        """
+        Salvar a base de dados treinada para evitar reprocessamento (salva em binário)
+        """
+        with open(FILE_DADOS_TREINADOS, 'wb') as f:
+            pickle.dump([x_atributos, y_classes, model, encoder], f)
 
-  def loadDataTrain():
-    """
-    Carregar a base de dados treinada
-    """
-    with open('modelo.pickle', 'rb') as f:
-      X_attr_train, Y_class_train = pickle.load(f)
-    return X_attr_train, Y_class_train
-
-
-
-class Naive_bayes:
-  
-
-  def splitData(dataBase):
-    """
-    Separar os dados entre um array atributos e um array classes
-    """
-    X_attr_train = dataBase.iloc[:, 0:4].values   # variavel de treinamento
-    X_attr_test = dataBase.iloc[:, 0:4].values   # variavel para testar o modelo (acuracia)
-
-    Y_class_train = dataBase.iloc[:, 4].values
-    Y_class_test = dataBase.iloc[:, 4].values
+    @staticmethod
+    def loadDataTrain():
+        """
+        Carregar a base de dados treinada
+        """
+        with open(FILE_DADOS_TREINADOS, 'rb') as f:
+            x_atributos, y_classes, modelTrained, encoder = pickle.load(f)
+        return x_atributos, y_classes, modelTrained, encoder
 
 
-  def codeToBynary():
-    """
-    Realizar conversão de atributos contínuos para discretos (converteR para binário)
-    """
-    encoder = OneHotEncoder()
-    X_attr_train = encoder.fit_transform(X_attr_train).toarray()
+class NaiveBayes:
+    x_atributos = None
+    y_classes = None
 
+    def set_x_atributos(self, dados):
+        self.x_atributos = dados
 
-  def training():
-    """
-    Realizar treinamento
-    """ 
-    naive_tennis = GaussianNB()
-    naive_tennis.fit(X_attr_train, Y_class_train)
+    def set_y_classes(self, dados):
+        self.y_classes = dados
 
+    def get_x_atributos(self):
+        return self.x_atributos
 
-  def predict():
-    """
-    Realizar uma inferência / previsão
-    """
-    X_teste = np.array([['sunny', 'mild', 'high', 'false ']])
-    X_teste_encoded = encoder.transform(X_teste).toarray()
-    y_pred = naive_tennis.predict(X_teste_encoded)
-    return y_pred
+    def get_y_classes(self):
+        return self.y_classes
 
+    def splitData(self, database):
+        """
+        Separar os dados entre um array atributos e um array classes
+        """
+        self.x_atributos = database.iloc[:, 0:4].values
+        self.y_classes = database.iloc[:, 4].values
 
-  def acccuracy(saidasPrevistas, saidasReais):
-    """
-    Calcular a acurácia do modelo
-    """
-    return accuracy_score(saidasReais, saidasPrevistas)
-  
-  def precision(saidasPrevistas, saidasReais):
-    """
-    Calcular a precisão do modelo
-    """
-    # pos_label: qual classe queremos calcular a precisão
-    return precision_score(saidasReais, saidasPrevistas, pos_label='yes') 
+    def codeToBynary(self):
+        """
+        Realizar conversão de atributos contínuos para discretos (converter para binário)
+        """
+        encoder = OneHotEncoder()
+        self.x_atributos = encoder.fit_transform(self.x_atributos).toarray()
+        return encoder
 
-  def recall(saidasPrevistas, saidasReais):
-    """
-    Calcular a sensibilidade do modelo
-    """
-    return recall_score(saidasReais, saidasPrevistas, pos_label='yes')
-  
-  def f1_score(saidasPrevistas, saidasReais):
-    """
-    Calcular o F1-Score do modelo
-    """
-    return f1_score(saidasReais, saidasPrevistas, pos_label='yes')
+    def training(self):
+        """
+        Realizar treinamento
+        """
+        model = GaussianNB()
+        model.fit(self.x_atributos, self.y_classes)
+        return model
 
-  def auc(saidasPrevistas, saidasReais, dadosTreinamento):
-    """
-    Calcular o auc do modelo
-    """
-    y_scores = naive_tennis.predict_proba(dadosTreinamento)[:, 1]  # obter as probabilidades de classe positiva
-    return roc_auc_score(saidasReais, y_scores)
+    @staticmethod
+    def doPredict(model_trained, encoder):
+        """
+        Realizar uma inferência / previsão
+        """
+        x_input = np.array([['sunny', 'mild', 'high', 'false ']])
+        x_input_encoded = encoder.transform(x_input).toarray()
+        y_pred = model_trained.predict(x_input_encoded)
+        return y_pred
+
+    @staticmethod
+    def acccuracy(saidas_previstas, saidas_reais):
+        """
+        Calcular a acurácia do modelo
+        """
+        return accuracy_score(saidas_reais, saidas_previstas) * 100
+
+    @staticmethod
+    def precision(saidas_previstas, saidas_reais):
+        """
+        Calcular a precisão do modelo
+        """
+        # pos_label: qual classe queremos calcular a precisão
+        return precision_score(saidas_reais, saidas_previstas, pos_label='yes') * 100
+
+    @staticmethod
+    def recall(saidas_previstas, saidas_reais):
+        """
+        Calcular a sensibilidade do modelo
+        """
+        return recall_score(saidas_reais, saidas_previstas, pos_label='yes') * 100
+
+    @staticmethod
+    def f1_score(saidas_previstas, saidas_reais):
+        """
+        Calcular o F1-Score do modelo
+        """
+        return f1_score(saidas_reais, saidas_previstas, pos_label='yes') * 100
+
+    @staticmethod
+    def auc(saidas_reais, dados_treinamento, model):
+        """
+        Calcular o auc do modelo
+        """
+        y_scores = model.predict_proba(dados_treinamento)[:, 1]  # obter as probabilidades de classe positiva
+        return roc_auc_score(saidas_reais, y_scores) * 100
 
 
 def executeTraining():
-  dataBase = readDataCSV()
-  X_attr_train, X_attr_test = splitData(dataBase)
+    naiveB = NaiveBayes()
+    io = IO()
 
-
-
+    dataBase = io.readDataCSV()
+    naiveB.splitData(dataBase)
+    encoder = naiveB.codeToBynary()
+    modelTrained = naiveB.training()
+    io.salveDataTrain(naiveB.get_x_atributos(), naiveB.get_y_classes(), modelTrained, encoder)
 
 
 def executePredict():
+    naiveB = NaiveBayes()
+    io = IO()
+
+    x_atributos, y_classes, modelTrained, encoder = io.loadDataTrain()
+    naiveB.set_x_atributos(x_atributos)
+    naiveB.set_y_classes(y_classes)
+
+    print("Output: ", naiveB.doPredict(modelTrained, encoder))
+
+    # Métricas
+    y_pred = modelTrained.predict(naiveB.get_x_atributos())  # Faz previsões no conjunto de teste
+    print("Acuracia: {:.1f}%".format(naiveB.acccuracy(y_pred, naiveB.get_y_classes())))
+    print("Precisao: {:.1f}%".format(naiveB.precision(y_pred, naiveB.get_y_classes())))
+    print("Sensibilidade: {:.1f}%".format(naiveB.recall(y_pred, naiveB.get_y_classes())))
+    print("F1-Score: {:.1f}%".format(naiveB.f1_score(y_pred, naiveB.get_y_classes())))
+    print("AUC: {:.1f}%".format(naiveB.auc(naiveB.get_y_classes(), naiveB.get_x_atributos(), modelTrained)))
 
 
-
-# Métricas
-
-X_attr_t = encoder.fit_transform(X_attr_test).toarray()  # Converter para binário
-y_pred = naive_tennis.predict(X_attr_t)   # Faz previsões no conjunto de teste
-
-
-
+# executeTraining()
+executePredict()

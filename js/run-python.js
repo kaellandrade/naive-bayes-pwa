0c617917488652ad438dc48dc3434443c6738d23
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2023-04-30 21:13:24 -0300

    ajustes em js e html puros

--- js/run-python.js
+++ js/run-python.js
@@ -4,15 +4,12 @@ async function main() {
 }
 
 let pyodideSobscribePromisse = main();
-const context = pyodideSobscribePromisse.globals;
-
-console.log(context);
 
 async function evaluatePython() {
   let pyodide = await pyodideSobscribePromisse;
   try {
     let python_code = await (await fetch("/python/main.py")).text();
-    pyodide.runPython(python_code);
+    pyodide.runPython(python_code, pyodide);
   } catch (err) {
     console.log(err);
   }
--- python/main.py
+++ python/main.py
@@ -2,15 +2,20 @@ import json
 import asyncio
 from js import console, fetch, document, window
 
-import js
+from pyodide.ffi import create_proxy # Cria ao proxy 
 
 
 url = "https://api.agify.io?name=meelad"
 
-async def getData():
+async def getData(*args):
     response = await fetch(url, {'method': 'GET'})
     json = await response.json()
     console.log('json', json)
+    pre_element = document.createElement("h2")
+    pre_element.textContent = str(json.age)
+    my_div = document.getElementById("result")
+    my_div.appendChild(pre_element)
+    return json
     
 
 async def main():
@@ -18,3 +23,24 @@ async def main():
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
+
+
+#  =================================================
+
+def f(*args):
+    inputValues = document.getElementById('input-id').value
+    console.log(inputValues)
+    
+def getValue():
+    btn = document.getElementById('button-id')
+
+    proxy_f = create_proxy(f, roundtrip=True)
+    btn = document.getElementById('button-id')
+    btn.addEventListener('click', proxy_f)
+    
+
+getValue()
+
+
+# btn.removeEventListener('click', proxy_f)
+# proxy_f.destroy()

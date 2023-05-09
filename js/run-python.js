5a7108a39826527ce377b88245d210f3da074214
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2023-05-09 02:26:34 -0300

    configurando loader

--- js/run-python.js
+++ js/run-python.js
@@ -1,19 +1,28 @@
 async function main() {
   let pyodide = await loadPyodide();
-  await pyodide.loadPackage("pandas")
+  await pyodide.loadPackage("pandas");
   return pyodide;
-  
 }
 
 let pyodideSobscribePromisse = main();
 
 async function evaluatePython() {
+  let loader = document.getElementById("loader");
+  let formHome = document.getElementById("fomulario-home");
+
+  loader.style.display = "block";
+  formHome.style.filter = "blur(.2rem)";
+  console.log(loader);
   let pyodide = await pyodideSobscribePromisse;
   try {
     let python_code = await (await fetch("/python/main.py")).text();
     pyodide.runPython(python_code, pyodide);
+    console.log("Fim");
   } catch (err) {
     console.log(err);
+  } finally {
+    loader.style.display = "none";
+    formHome.style.filter = "blur(0)";
   }
 }
 evaluatePython();
